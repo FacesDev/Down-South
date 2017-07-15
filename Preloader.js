1cdba657fb5bec9910a742ce1109d74f0318bd84
Chris <kitecabrinha@gmail.com>
2017-07-15 15:34:24 -0400

    First commit

---
+++ Preloader.js
@@ -0,0 +1,26 @@
+Game.Preloader = function(game){
+     this.preloadBar = null;
+};
+
+Game.Preloader.prototype = {
+    preload:function(){
+
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
+
+        this.preloadBar.anchor.setTo(0.5,0.5);
+
+        this.time.advancedTiming = true;
+
+        this.load.setPreloadSprite(this.preloadBar);
+        console.log('text');
+        this.load.tilemap('map', 'assets/Level1.csv', null, Phaser.Tilemap.CSV);
+        this.load.image('tileset', 'assets/ts.png');
+    },
+
+
+create:function(){
+
+    this.state.start('Level1');
+
+}
+}
---
+++ assets/preloader.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tsNew" tilewidth="16" tileheight="16" tilecount="870" columns="29">
+ <image source="assets/ts.png" width="472" height="480"/>
+</tileset>
